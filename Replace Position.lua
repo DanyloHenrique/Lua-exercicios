290b1a936261623fd985a025c85c7f24a25f2efe
Danylo Henrique <85462660+DanyloHenrique@users.noreply.github.com>
2024-09-14 07:15:33 -0300

    Update Replace Position.lua

--- Replace Position.lua
+++ Replace Position.lua
@@ -1,3 +1,13 @@
+-- Exercício: Replace With Alphabet Position (https://www.codewars.com/kata/546f922b54af40e1e90001da)
+-- Descrição:
+-- Neste exercício, você deve pegar cada letra de uma string e substituir
+-- por sua posição correspondente no alfabeto. Ignore caracteres que não sejam letras.
+-- A = 1, B = 2, C = 3, etc.
+
+-- Exemplo:
+-- Input: "The sunset sets at twelve o' clock."
+-- Output: "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
 local function alphabet_position(text)
   
   -- inicia um array de alfabeto para auxiliar na contagem e uma var de resultado onde os numeros serao armazenados e retornad
